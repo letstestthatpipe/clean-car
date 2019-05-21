9b7cfb6a80e131c5f4925832f5d61346eb6a9e05
Basem Vaseghi <basem.vaseghi@daimler.com>
2019-05-21 21:26:21 +0200

    swagger for base functionality

--- go.mod
+++ go.mod
@@ -2,7 +2,4 @@ module github.com/vbasem/clean-car
 
 go 1.12
 
-require (
-	github.com/go-chi/chi v4.0.2+incompatible
-	golang.org/x/oauth2 v0.0.0-20190517181255-950ef44c6e07
-)
+require github.com/go-chi/chi v4.0.2+incompatible
--- main.go
+++ main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"github.com/go-chi/chi"
-	"github.com/vbasem/clean-car/handlers"
-	"github.com/vbasem/clean-car/infra"
 	"log"
 	"net/http"
 	"os"
 	"path/filepath"
+
+	"github.com/go-chi/chi"
+	"github.com/vbasem/clean-car/infra"
 )
 
 func main() {
@@ -21,21 +21,8 @@ func main() {
 	}
 
 	swaggerDir := filepath.Join(workDir, "swaggerui")
-	webappDir := filepath.Join(workDir, "webapp")
 
 	infra.FileServer(r, "/swagger-ui", http.Dir(swaggerDir))
-	infra.FileServer(r, "/webapp", http.Dir(webappDir))
-
-	r.Get("/api/dummy", handlers.DummyApi)
-	r.Get("/api/car/{carId}", handlers.GetCarStatus)
-
-	r.Get("/", handleMain)
-	r.Get("/login", infra.HandleOauthLogin)
-	r.Get("/callback", handlers.HandleToggleCarDoors)
 
 	http.ListenAndServe(":3333", r)
-}
-
-func handleMain(w http.ResponseWriter, r *http.Request) {
-	http.Redirect(w, r, "webapp", http.StatusSeeOther)
 }
